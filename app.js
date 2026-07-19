2bcd6d4ead53ffd866d65420b55514c9848ffc5f
tharindu-weragama <tharindu.weragama98@gmail.com>
2026-07-20 02:31:56 +0530

    Implement customer class with properties and instance creation

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("hello!");
+class customer {
+    name;
+    age;
+    address;
+
+
+    constructor(name, age, address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+
+    }
+
+}
+
+let customer1 = new customer("THARINDU", 20, "COLOMBO");
+console.log(customer1);
+
