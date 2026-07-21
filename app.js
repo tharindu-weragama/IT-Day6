26e588aef9fbc9e2a6d424accce868a558597ee9
tharindu-weragama <tharindu.weragama98@gmail.com>
2026-07-21 23:04:28 +0530

    Add customer2 object with detailed properties and logging

--- app.js
+++ app.js
@@ -16,3 +16,51 @@ class customer {
 let customer1 = new customer("THARINDU", 20, "COLOMBO");
 console.log(customer1);
 
+let customer2 = {
+    name: "John",
+    age: 23,
+    address: 'new york',
+    salary: 40000,
+    item: [
+        {
+            id : 1,
+            name : "item1",
+            price : 100
+        }, 
+        {
+            id : 2,
+            name : "item2",
+            price : 200
+        },
+        {
+            id : 3,
+            name : "item3",
+            price : 300,
+            variants : [
+                {
+                    id : 1,
+            name : "variety1",
+            price : 10,
+                },
+                {
+                    id : 2,
+            name : "variety",
+            price : 20,
+                }
+            ]
+        } ],
+    father: {
+        name: "dpoe",
+        age: 50,
+        address: "new yrk"
+    }
+};
+
+console.log(customer2.name);
+console.log(customer2.age);
+console.log(customer2.address);
+console.log(customer2.salary);
+console.log(customer2.item[2]);
+console.log(customer2.father.name);
+console.log(customer2.item[2].variants[1].price);
+
